e927d0220d63179ee28ae97df3bef6047355eb6b
ajit8874 <78336358+ajit8874@users.noreply.github.com>
2022-02-22 16:11:41 +0530

    Add files via upload

--- blockChain.py
+++ blockChain.py
@@ -2,8 +2,6 @@ import datetime
 import hashlib
 from webbrowser import get
 
-
-
 def calc_hash():
       sha = hashlib.sha256()
 
@@ -12,11 +10,7 @@ def calc_hash():
       sha.update(hash_str)
 
       return sha.hexdigest()
-
-
 class Block:
-
-
     def __init__(self, timestamp, data, previous_hash):
 
         self.timestamp = timestamp
@@ -25,17 +19,13 @@ class Block:
         self.hash = calc_hash()
         self.next=None
 
-
 def get_time_stamp():
     return datetime.datetime.now()
-
 
 class BlockChain:
     def __init__(self):
         self.head=None
-        self.tail=None
-      
-    
+        # self.tail=None
 
     def append(self,data):
 
@@ -47,35 +37,47 @@ class BlockChain:
         else:
             while self.head.next is not None:
                 self.head=self.head.next
+            
             self.head.next=Block(get_time_stamp(),data,self.head.hash)
-         
         
-        # if self.head.next is not None:
-        #     self.head=self.head.next
-        # else:
+        
 
-        #     self.head.next=Block(get_time_stamp,value,0)
-        
-        
-    
-
-    def to_str(self):
+    def to_print(self):
         output=[]
+        node=self.head
+        index_no=0
         if self.head is not None:
             output.append(self.head)
             self.head=self.head.next
-        return output
+        
+        print("Index No: ",index_no)
+        print("TimeStamp: ",str(node.timestamp))
+        print("Date: ", node.data)
+        print("SHA256 hASH: ",node.hash)
+        print("previous hash: ",node.previous_hash)
+
+        node =node.next
+        count +=1
+
+        print("\n")
+        # return output
 
 
 
 
             
 
-blockchain=BlockChain()
-blockchain.append("abc")
-blockchain.append("def")
-blockchain.append("ghi")
+blockchain1=BlockChain()
+print("block chain",blockchain1)
 
-for i in blockchain.to_str():
-    print(i,"\n")
+blockchain1.to_print()
+
+blockchain1.append("abc")
+blockchain1.append("def")
+blockchain1.append("ghi")
+
+blockchain1.to_print()
+
+# for i in blockchain.to_str():
+#     print(i,"\n")
 
